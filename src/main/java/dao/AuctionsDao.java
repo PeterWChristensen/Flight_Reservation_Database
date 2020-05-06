0fece70e8ede00c4a54a657126a8160c61444822
PeterWChristensen <47579035+PeterWChristensen@users.noreply.github.com>
2020-05-06 13:24:07 -0400

    Add files via upload

--- src/main/java/dao/AuctionsDao.java
+++ src/main/java/dao/AuctionsDao.java
@@ -1,4 +1,6 @@
 package dao;
+import java.sql.*;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,16 +17,34 @@ public class AuctionsDao {
 		 * The Auctions record is required to be encapsulated as a "Auctions" class object
 		 */
 		List<Auctions> auctions = new ArrayList<Auctions>();
-		/*Sample data begins*/
-		Auctions auction = new Auctions();
-		auction.setAccountNo(AccountNo);
-		auction.setAirlineID(AirlineID);
-		auction.setFlightNo(FlightNo);
-		auction.setSeatClass(SeatClass);
-		auction.setAccepted(true);
-		auction.setDate("2019-01-01");
-		auction.setNYOP(500);
-		auctions.add(auction);
+		/*Sample data begins*/		
+		String query = "SELECT * FROM Auctions WHERE AccountNo = ? AND AirlineID = ? AND FlightNo = ? AND Class = ?"
+				+ "	ORDER BY `Date` DESC LIMIT 0,1";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql4.cs.stonybrook.edu:3306/pwchristense", "pwchristense", "112123806");
+			PreparedStatement st = con.prepareStatement(query);
+			st.setInt(1, AccountNo);
+			st.setString(2, AirlineID);
+			st.setInt(3, FlightNo);
+			st.setString(4, SeatClass);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				Auctions auction = new Auctions();
+				auction.setAccountNo(rs.getInt("AccountNo"));
+				auction.setAirlineID(rs.getString("AirlineID"));
+				auction.setFlightNo(rs.getInt("FlightNo"));
+				auction.setSeatClass(rs.getString("Class"));
+				auction.setAccepted(true);
+				auction.setDate(rs.getString("Date"));
+				auction.setNYOP(rs.getDouble("NYOP"));
+				auctions.add(auction);
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 		/*Sample data ends*/
 		
 		return auctions;
@@ -40,18 +60,32 @@ public class AuctionsDao {
 		List<Auctions> auctions = new ArrayList<Auctions>();
 			
 		/*Sample data begins*/
-		for (int i = 0; i < 4; i++) {
-			Auctions auction = new Auctions();
-			auction.setAccountNo(AccountNo);
-			auction.setAirlineID(AirlineID);
-			auction.setFlightNo(FlightNo);
-			auction.setSeatClass(SeatClass);
-			auction.setAccepted(true);
-			auction.setDate("2019-01-01");
-			auction.setNYOP(500);
-	
-			auctions.add(auction);
-				
+		String query = "SELECT * FROM Auctions WHERE AccountNo = ? AND AirlineID = ? AND FlightNo = ? AND Class = ?"
+				+ "	ORDER BY `Date` DESC";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql4.cs.stonybrook.edu:3306/pwchristense", "pwchristense", "112123806");
+			PreparedStatement st = con.prepareStatement(query);
+			st.setInt(1, AccountNo);
+			st.setString(2, AirlineID);
+			st.setInt(3, FlightNo);
+			st.setString(4, SeatClass);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				Auctions auction = new Auctions();
+				auction.setAccountNo(rs.getInt("AccountNo"));
+				auction.setAirlineID(rs.getString("AirlineID"));
+				auction.setFlightNo(rs.getInt("FlightNo"));
+				auction.setSeatClass(rs.getString("Class"));
+				auction.setAccepted(true);
+				auction.setDate(rs.getString("Date"));
+				auction.setNYOP(rs.getDouble("NYOP"));
+				auctions.add(auction);
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
 		}
 		/*Sample data ends*/
 						
--- src/main/java/dao/BookReservationDao.java
+++ src/main/java/dao/BookReservationDao.java
@@ -1,7 +1,16 @@
 package dao;
+import java.sql.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
+import model.Auctions;
 import model.BookReservation;
 
 public class BookReservationDao {
@@ -16,8 +25,88 @@ public class BookReservationDao {
 		 * The sample code returns "success" by default.
 		 * You need to handle the database deletion and return "success" or "failure" based on result of the database deletion.
 		 */
+		
+		String query = "INSERT INTO Reservation VALUES (?, ?, ?, ?, ?, ?); ";
+		String query2 = "INSERT INTO Includes VALUES (?, ?, ?, ?, ?); "; 
+		String query3 = "INSERT INTO ReservationPassenger VALUES(?, ?, ?, ?, ?, ?); ";
+		
+		String findResNo = "SELECT MAX(ResrNo) FROM Reservation";
+		
+		String query4 = "INSERT INTO Includes VALUES (?, ?, ?, ?, ?);";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql4.cs.stonybrook.edu:3306/pwchristense", "pwchristense", "112123806");
+			PreparedStatement st = con.prepareStatement(query);
+			Statement statement = con.createStatement();
+			ResultSet resno = statement.executeQuery(findResNo);
+			resno.next();
+			int resNo = resno.getInt(1) + 1;
+			if (!(resNo > 0)) {
+				resNo = 0;
+			}
+			
+			st.setInt(1, resNo);
+			Date date = new Date();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+			String strDate = dateFormat.format(date);  
+			st.setString(2, strDate);
+			st.setInt(3, 400);
+			st.setInt(4, 1000);
+			if (bookRes.getRepSSN() != null && bookRes.getRepSSN() != "" )
+				st.setInt(5, Integer.parseInt(bookRes.getRepSSN()));
+			else 
+				st.setNull(5, Types.NULL);
+			st.setInt(6, 1111);
+			boolean success = true;
+			int result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+			
+			st = con.prepareStatement(query2);
+			st.setInt(1,  resNo);
+			st.setString(2, bookRes.getAirlineID());
+			st.setInt(3, bookRes.getFlightNum1());
+			st.setInt(4, 1);
+			st.setString(5, bookRes.getDepartureDate());
+			result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+			
+			st = con.prepareStatement(query3);
+			st.setInt(1, resNo);
+			st.setInt(2, 111);
+			st.setInt(3, 1111);
+			st.setString(4,  bookRes.getSeatNum());
+			st.setString(5, bookRes.getSeatClass());
+			st.setString(6,  bookRes.getMealPref());
+			result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+
+			
+			if (bookRes.getTypeOfTrip() == "Round-trip") {
+				st = con.prepareStatement(query3);
+				st.setInt(1, resNo);
+				st.setString(2, bookRes.getAirlineID());
+				st.setInt(3, bookRes.getFlightNum2());
+				st.setInt(4, 1);
+				st.setString(5,  bookRes.getReturnDate());
+				result = st.executeUpdate();
+				if (result <= 0)
+					success = false;
+			}
+			
+			if (success)
+				return "success";
+			else
+				return "failure";
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 					
-		return "success";
+		return "failure";
 		
 	}
 	
@@ -33,7 +122,84 @@ public class BookReservationDao {
 		 * You need to handle the database deletion and return "success" or "failure" based on result of the database deletion.
 		 */
 					
-		return "success";
+		String query = "INSERT INTO Reservation VALUES (?, ?, ?, ?, ?, ?); ";
+		String query2 = "INSERT INTO Includes VALUES (?, ?, ?, ?, ?); "; 
+		String query3 = "INSERT INTO ReservationPassenger VALUES(?, ?, ?, ?, ?, ?); ";
+		
+		String findResNo = "SELECT MAX(ResrNo) FROM Reservation";
+		
+		String query4 = "INSERT INTO Includes VALUES (?, ?, ?, ?, ?);";
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://mysql4.cs.stonybrook.edu:3306/pwchristense", "pwchristense", "112123806");
+			PreparedStatement st = con.prepareStatement(query);
+			Statement statement = con.createStatement();
+			ResultSet resno = statement.executeQuery(findResNo);
+			resno.next();
+			int resNo = resno.getInt(1) + 1;
+			if (!(resNo > 0)) {
+				resNo = 0;
+			}
+			
+			st.setInt(1, resNo);
+			Date date = new Date();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+			String strDate = dateFormat.format(date);  
+			st.setString(2, strDate);
+			st.setInt(3, 400);
+			st.setInt(4, 1000);
+			if (bookRes.getRepSSN() != null && bookRes.getRepSSN() != "" )
+				st.setInt(5, Integer.parseInt(bookRes.getRepSSN()));
+			else 
+				st.setNull(5, Types.NULL);
+			st.setInt(6, 1111);
+			boolean success = true;
+			int result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+			
+			st = con.prepareStatement(query2);
+			st.setInt(1,  resNo);
+			st.setString(2, bookRes.getAirlineID());
+			st.setInt(3, bookRes.getFlightNum1());
+			st.setInt(4, 1);
+			st.setString(5, bookRes.getTrip1Date());
+			result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+			
+			st = con.prepareStatement(query3);
+			st.setInt(1, resNo);
+			st.setInt(2, 111);
+			st.setInt(3, 1111);
+			st.setString(4,  bookRes.getSeatNum());
+			st.setString(5, bookRes.getSeatClass());
+			st.setString(6,  bookRes.getMealPref());
+			result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+
+			st = con.prepareStatement(query3);
+			st.setInt(1, resNo);
+			st.setString(2, bookRes.getAirlineID());
+			st.setInt(3, bookRes.getFlightNum2());
+			st.setInt(4, 1);
+			st.setString(5,  bookRes.getTrip2Date());
+			result = st.executeUpdate();
+			if (result <= 0)
+				success = false;
+			
+			if (success)
+				return "success";
+			else
+				return "failure";
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+					
+		return "failure";
 		
 	}
 }
